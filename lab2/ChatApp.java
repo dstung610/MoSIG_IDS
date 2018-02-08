fd34b9005b6e65512642214b549c5d90ab3c5f5a
Phu HUYNH <hdcphu@hotmail.com>
2018-02-08 15:58:15 +0100

    +add draft code for chating service

--- lab2/ChatApp.java
+++ lab2/ChatApp.java
@@ -1,6 +1,7 @@
 import java.rmi.*;
 
 public interface ChatApp extends Remote {
+	static public final s_Command_Quit = "@-QUIT";
 	public String joinChatRoom()  throws RemoteException;
 	public String leaveChatRoom() throws RemoteException;
 	public String saySomething(String) throws RemoteException;
